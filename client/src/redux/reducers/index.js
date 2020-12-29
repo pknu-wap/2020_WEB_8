0c69bbd94b042c7639e86a67669fb6c5c80563e5
jcw519 <chaewonjeong.dev@gmail.com>
2020-12-29 17:13:29 +0900

    PostCardList(1)

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,11 +1,14 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import authReducer from "./authReducer";
+import postReducer from "./postReducer.js";
+
 
 const createRootReducer = (history) =>
   combineReducers({
     router: connectRouter(history),
     auth: authReducer,
+    post: postReducer,
   });
 
 export default createRootReducer;
--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -3,10 +3,14 @@ import axios from "axios";
 
 import authSaga from "./authSaga";
 import dotenv from "dotenv";
+import postSaga from "./postSaga";
 dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL;
 
 export default function* rootSaga() {
-  yield all([fork(authSaga)]);
+  yield all([
+    fork(authSaga),
+    fork(postSaga)
+  ]);
 }
